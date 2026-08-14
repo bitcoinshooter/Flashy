4cb0994208a681f9f15eea3ad298cfe58b6d6058
mike <mike@brosinink.com>
2026-08-14 14:35:02 -0700

    Initial Commit

---
+++ FLASH/FLASHApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLASHApp.swift
+//  FLASH
+//
+//  Created by Mike on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct FLASHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
